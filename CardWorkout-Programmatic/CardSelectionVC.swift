f5fabf470207d4da5728b31ef4c4c7766ebc86fb
murad <muradkv@gmail.com>
2022-08-08 00:52:47 +0300

    Configure window, add root ViewController

---
+++ CardWorkout-Programmatic/CardSelectionVC.swift
@@ -0,0 +1,18 @@
+//
+//  CardSelectionVC.swift
+//  CardWorkout-Programmatic
+//
+//  Created by murad on 08.08.2022.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .brown
+    }
+}
