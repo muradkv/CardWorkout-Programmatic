d5c6e4a7a63c7a049f7977bd0f587cb19db05def
murad <muradkv@gmail.com>
2022-08-10 19:06:00 +0300

    Add Deck struct with all values

---
+++ CardWorkout-Programmatic/Deck.swift
@@ -0,0 +1,69 @@
+//
+//  Deck.swift
+//  CardWorkout-Programmatic
+//
+//  Created by murad on 10.08.2022.
+//
+
+import UIKit
+
+struct Deck {
+    static let allValues = [
+        UIImage(named: "2H")!,
+        UIImage(named: "3H")!,
+        UIImage(named: "4H")!,
+        UIImage(named: "5H")!,
+        UIImage(named: "6H")!,
+        UIImage(named: "7H")!,
+        UIImage(named: "8H")!,
+        UIImage(named: "9H")!,
+        UIImage(named: "10H")!,
+        UIImage(named: "AH")!,
+        UIImage(named: "KH")!,
+        UIImage(named: "QH")!,
+        UIImage(named: "JH")!,
+        
+        UIImage(named: "2C")!,
+        UIImage(named: "3C")!,
+        UIImage(named: "4C")!,
+        UIImage(named: "5C")!,
+        UIImage(named: "6C")!,
+        UIImage(named: "7C")!,
+        UIImage(named: "8C")!,
+        UIImage(named: "9C")!,
+        UIImage(named: "10C")!,
+        UIImage(named: "AC")!,
+        UIImage(named: "KC")!,
+        UIImage(named: "QC")!,
+        UIImage(named: "JC")!,
+        
+        UIImage(named: "2S")!,
+        UIImage(named: "3S")!,
+        UIImage(named: "4S")!,
+        UIImage(named: "5S")!,
+        UIImage(named: "6S")!,
+        UIImage(named: "7S")!,
+        UIImage(named: "8S")!,
+        UIImage(named: "9S")!,
+        UIImage(named: "10S")!,
+        UIImage(named: "AS")!,
+        UIImage(named: "KS")!,
+        UIImage(named: "QS")!,
+        UIImage(named: "JS")!,
+        
+        UIImage(named: "2D")!,
+        UIImage(named: "3D")!,
+        UIImage(named: "4D")!,
+        UIImage(named: "5D")!,
+        UIImage(named: "6D")!,
+        UIImage(named: "7D")!,
+        UIImage(named: "8D")!,
+        UIImage(named: "9D")!,
+        UIImage(named: "10D")!,
+        UIImage(named: "AD")!,
+        UIImage(named: "KD")!,
+        UIImage(named: "QD")!,
+        UIImage(named: "JD")!
+    ]
+}
+
