d66920cdd4b99f0dc3dc422ba075cee7eb6e3d79
murad <muradkv@gmail.com>
2022-08-10 18:06:16 +0300

    Transition to RulesVC when button tapped

---
+++ CardWorkout-Programmatic/RulesVC.swift
@@ -0,0 +1,18 @@
+//
+//  RulesVC.swift
+//  CardWorkout-Programmatic
+//
+//  Created by murad on 10.08.2022.
+//
+
+import UIKit
+
+class RulesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+    
+}
